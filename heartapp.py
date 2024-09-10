d8f152372f0fb52f5b79fed006b27fb885068ef2
Nihalnsg <nihal.gunathilaka@neosolax.com>
2024-09-10 10:34:39 +0530

    test cicd

--- heartapp.py
+++ heartapp.py
@@ -34,5 +34,29 @@ def heart_type(data:heartuser):
         'prediction': prediction[0]
     }
 
+
+@app.post('/heart/predict/test')
+def heart_type_test(data: heartuser):
+    data = data.dict()
+    age = data['age']
+    sex = data['sex']
+    cp = data['cp']
+    trtbps = data['trtbps']
+    chol = data['chol']
+    fbs = data['fbs']
+    restecg = data['restecg']
+    thalachh = data['thalachh']
+    exng = data['exng']
+    oldpeak = data['oldpeak']
+    slp = data['slp']
+    caa = data['caa']
+    thal = data['thal']
+    prediction = model.predict([[age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thal]])
+
+    return {
+        'prediction': prediction[0]
+    }
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
